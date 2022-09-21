dfed19b7290e4dfaf61ef5b8a73a664d1323ef03
Ricardo Moreira <calditos1988@outlook.com>
2022-09-20 21:21:18 -0300

    criando os primeiros componentes e estilos

---
+++ src/components/layouts/Conteudo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Conteúdo</h1>
+        <home></home>
+        <publicar-vaga></publicar-vaga>
+    </div>
+</template>
+
+<script>
+import Home from '@/components/views/Home.vue'
+import PublicarVaga from '@/components/views/PublicarVaga.vue' 
+export default {
+    name : 'Conteudo',
+    components: {
+        Home,
+        PublicarVaga
+    }
+}
+</script>
+
+<style scoped>
+    h1 {
+    color: blue;
+    }
+</style>
---
+++ src/components/views/PublicarVaga.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Publicar vaga</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Publicar Vaga'
+}
+</script>
+
+<style>
+
+</style>
